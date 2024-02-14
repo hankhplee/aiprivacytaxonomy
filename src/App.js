50fe9c02a7040d3eae8570c162fdc0216f7d3954
MonkeyJerr <ajm3@andrew.cmu.edu>
2024-02-14 14:59:15 -0500

    initial placeholder page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const pdfUrl = "https://arxiv.org/pdf/2310.07879.pdf";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>AI Taxonomy Project</h1>
+        <p>Full Website Coming Soon...</p>
+        <div>
+          <a href={pdfUrl} target="_blank" rel="noopener noreferrer" className="pdf-link">{pdfUrl}</a>
+        </div>
+        <div className = "pdf-container">
+          {/* You can display the PDF using an iframe */}
+          {pdfUrl && (
+            <iframe
+              title="PDF Viewer"
+              src={pdfUrl}
+              width="100%"
+              height="1000"
+              style={{ border: '1px solid black' }}
+            ></iframe>
+          )}
+        </div>
       </header>
     </div>
   );
