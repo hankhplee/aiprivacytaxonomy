de83fdd72d3f24850314ddcd0f094d3314474cdf
Hank Lee <hankhplee@gmail.com>
2024-04-18 17:33:40 -0400

    update publication page format

--- src/publication.js
+++ src/publication.js
@@ -13,22 +13,26 @@ function Publication() {
         <div className="header-text">
           <div className="content">
             <h1 className="main-title">Publication</h1>
-            <h2 className="subtitle">The official paper behind this website, <br/>along with the people who made this possible</h2> {/* Removed bold */}
+            {/* <h2 className="subtitle">The official paper behind this website, <br/>along with the people who made this possible</h2> Removed bold */}
 
           </div>
         </div>
       </div>
 
       <div className="info-paragraph">
-        <h1 className="paragraph-title">Reference Info</h1> {/* Increased font size and made bold */}
-        <div className="blue-line"></div> {/* Blue line */}
+        {/* <h1 className="paragraph-title">Reference Info</h1> */}
+        {/* <div className="blue-line"></div> */}
+        <p><a href='https://arxiv.org/abs/2310.07879'>Deepfakes, Phrenology, Surveillance, and More! A Taxonomy of AI Privacy Risks</a></p>
+        <p><i><a href='https://chi2024.acm.org/'>CHI Conference on Human Factors in Computing Systems (CHI) 2024</a></i></p>
         <p>Hao-Ping (Hank) Lee, Yu-Ju Yang, Thomas Serban von Davier, Jodi Forlizzi, 
-            and Sauvik Das. 2024. Deepfakes, Phrenology, Surveillance, and More! 
-            A Taxonomy of AI Privacy Risks. In Proceedings of the CHI Conference on Human 
-            Factors in Computing Systems (CHI ’24), May 11–16, 2024, Honolulu, HI, USA. ACM, 
-            New York, NY, USA, 30 pages.</p>
-        <div>
+            and Sauvik Das</p>
+        {/* <div>
           <a href={pdfUrl} target="_blank" rel="noopener noreferrer" className="pdf-link">{pdfUrl}</a>
+        </div> */}
+        <div>
+          <p>
+          {"@inproceedings{lee2024aiprivacytaxonomy, title={Deepfakes, Phrenology, Surveillance, and More! A Taxonomy of AI Privacy Risks}, author={Lee, Hao-Ping (Hank) and Yang, Yu-Ju and von Davier, Thomas Serban and Forlizzi, Jodi and Das, Sauvik}, booktitle = {{{CHI Conference}} on {{Human Factors}} in {{Computing Systems}}}, year={2024} }"}
+          </p>
         </div>
         <div className = "pdf-container">
           {pdfUrl && (
